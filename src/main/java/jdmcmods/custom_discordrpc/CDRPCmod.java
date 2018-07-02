804912361d60f4e642d58a2fbb31000ec36e7cf1
Jaideep Singh Heer <jaideesinghheer@gmail.com>
2018-07-02 20:47:08 +0530

    - The mod is now supposed to try to load before all other mods for a faster response.

--- src/main/java/jdmcmods/custom_discordrpc/CDRPCmod.java
+++ src/main/java/jdmcmods/custom_discordrpc/CDRPCmod.java
@@ -16,6 +16,7 @@ import org.apache.logging.log4j.Logger;
         version = CDRPCmod.VERSION,
         canBeDeactivated = true,
         acceptedMinecraftVersions = "[1.11,)",
+        dependencies = "before:*;",
         updateJSON = CDRPCmod.UPDATE_JSON,
         clientSideOnly = true
 )
@@ -23,7 +24,7 @@ public class CDRPCmod {
 
     public static final String MOD_ID = "customdiscordrpc";
     public static final String MOD_NAME = "Custom Discord RPC";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final String UPDATE_JSON = "https://raw.githubusercontent.com/jaideepheer/MinecraftForge-custom_discordRPC/master/src/main/resources/update.json";
 
     public static Logger LOGGER = LogManager.getLogger(MOD_ID);
